5da94cb1580ee256fa69d5f5ab50f2c79850e398
AleksArden <i1723@i.ua>
2023-03-03 21:49:23 +0200

    add homework

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# node-hw-01
+# node-hw-01-CLI
+
+getList https://monosnap.com/file/mPrgI1H4viQHJJR1DicNUttonfrQgC
+
+getContact https://monosnap.com/file/IVtjSjQxbRVqpT5ePhFgUtwmBJiZGb
+
+addContact https://monosnap.com/file/ZWuabnMrr7jtNWbiDxtfwWjdSkiiYN
+
+removeContact https://monosnap.com/file/uNvmxHPFd0SwkVNcoU7ZVdPwAO57sZ
---
+++ contacts.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const fs = require("fs").promises;
+const { v4: uuidv4 } = require('uuid');
+
+const contactsPath = path.join("db", "contacts.json");
+/**
+ * Read list of contacts
+ * @returns {Promise<void>}
+ */
+async function listContacts() {
+    try {
+        const readResult = await fs.readFile(contactsPath);
+
+        const listContacts = JSON.parse(readResult);
+
+        console.table(listContacts)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+/**
+ * get contact by ID
+ * @param {string} contactId 
+ * @returns { Promise < void>}
+ */
+async function getContactById(contactId) {
+    try {
+        const readResult = await fs.readFile(contactsPath);
+
+        const listContacts = JSON.parse(readResult);
+        const idx = listContacts.findIndex(({ id }) => id === contactId);
+        const contact = listContacts[idx];
+
+        console.log(contact)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+/**
+ * remove contact by ID
+ * @param {string} contactId 
+ * @returns { Promise < void>}
+ */
+async function removeContact(contactId) {
+    try {
+        const readResult = await fs.readFile(contactsPath);
+
+        const listContacts = JSON.parse(readResult);
+        const newContacts = listContacts.filter(({ id }) => id !== contactId);
+
+        await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+/**
+ * add contact
+ * @param {string} name 
+ * @param {string} email 
+ * @param {string} phone 
+ * @returns { Promise < void>}
+ */
+async function addContact(name, email, phone) {
+    try {
+        const readResult = await fs.readFile(contactsPath);
+
+        const listContacts = JSON.parse(readResult);
+        const contact = {
+            id: uuidv4(),
+            name,
+            email,
+            phone,
+        };
+        const newContacts = JSON.stringify([...listContacts, contact]);
+
+        await fs.writeFile(contactsPath, newContacts);
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+}
